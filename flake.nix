4af97b0f6ffceb53dbe370fe6d8bc52eb55b339c
ervinerviansyah <ervin.erviansyah2003@gmail.com>
2025-02-27 15:21:26 +0700

    retry to fix flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "My Configuration of NixOS | ervin@nixos";
-
+  
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager = {
@@ -12,21 +12,40 @@
   outputs = { self, nixpkgs, home-manager, ... } @inputs:
   let
     system = "x86_64-linux";
-    homeStateVersion = "24.11";
     user = "ervin";
     hosts = [
-      { hostname = "nixos"; stateVersion = "24.11"; }
+      { hostname = "nixos"; }
     ];
-    pkgs = nixpkgs.legacyPackages.${system};
+
+    pkgs = nixpkgs.packages.${system};
+
+    makeSystem = { hostname }: 
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ({ ... }: {
+            system.stateVersion = "24.11";  # Versi langsung di sini
+            specialArgs = { inherit inputs user; };
+          })
+          ./hosts/${hostname}/configuration.nix
+        ];
+      };
 
   in {
+    nixosConfigurations = builtins.listToAttrs (
+      map (host: {
+        name = host.hostname;
+        value = makeSystem host;
+      }) hosts
+    );
+
     homeConfigurations.${user} = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
-      extraSpecialArgs = { };
+      home.stateVersion = "24.11";
+      extraSpecialArgs = { inherit inputs user; };
       modules = [
         ./home-manager/home.nix
       ];
     };
   };
-   
 }
