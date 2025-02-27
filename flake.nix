cb4509a03f7c8627f3e0995fbfe198cb1515b7b3
ervinerviansyah <ervin.erviansyah2003@gmail.com>
2025-02-27 14:16:50 +0700

    configuration flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,55 @@
 {
-  description = "A very basic flake";
+  description = "My Configuration of NixOS | ervin@nixos";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-24.11";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: let
+  system = "x86_64-linux";
+  homeStateVersion = "24.11";
+  user = "ervin";
+  hosts = [
+    { hostname = "nixos"; stateVersion = "24.11"; }
+  ];
+  
+  makeSystem = { hostname, stateVersion }: nixpkgs.lib.nixosSystem {
+      system = system;
+      specialArgs = {
+        inherit inputs stateVersion hostname user;
+      };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      modules = [
+        ./hosts/${hostname}/configuration.nix
+      ];
+    };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    in {
+    nixosConfigurations = nixpkgs.lib.foldl' (configs: host:
+      configs // {
+        "${host.hostname}" = makeSystem {
+          inherit (host) hostname stateVersion;
+        };
+      }) {} hosts;
 
+    homeConfigurations.${user} = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      extraSpecialArgs = {
+        inherit inputs homeStateVersion user;
+      };
+
+      modules = [
+        ./home-manager/home.nix
+      ];
+    };
   };
+  
+
 }
